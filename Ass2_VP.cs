496607fb59ab05dac4c49fdf8c0455e3cff6219a
M. <raomariamtanveer9@gmail.com>
2024-11-18 17:06:50 +0500

    Add files via upload

---
+++ Ass2_VP.cs
@@ -0,0 +1,25 @@
+namespace Assignment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is Message Box");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
